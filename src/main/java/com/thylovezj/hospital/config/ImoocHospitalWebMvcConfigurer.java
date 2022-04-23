34d85f2f013a203af795485f46ab505a9473d085
thylovezj <1105864486@qq.com>
2022-04-23 14:02:34 +0800

    THY20220423更新了Swagger2

---
+++ src/main/java/com/thylovezj/hospital/config/ImoocHospitalWebMvcConfigurer.java
@@ -0,0 +1,18 @@
+package com.thylovezj.hospital.config;
+
+import com.thylovezj.hospital.common.Constant;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ImoocHospitalWebMvcConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("images/**").addResourceLocations("file:" + Constant.FILE_UPLOAD_DIR);
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+    }
+}
