1c1178c8bb2112fcf21e471ff66c2910a520006f
thylovezj <1105864486@qq.com>
2022-05-03 14:11:19 +0800

    更新了问题提交模块代码

--- src/main/java/com/thylovezj/hospital/service/impl/CgcProblemServiceImpl.java
+++ src/main/java/com/thylovezj/hospital/service/impl/CgcProblemServiceImpl.java
@@ -22,6 +22,7 @@ import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Service
 @Slf4j
 public class CgcProblemServiceImpl extends ServiceImpl<CgcProblemMapper, CgcProblem> implements CgcProblemService {
